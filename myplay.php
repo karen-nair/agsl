48ec24df19f1cd412ff8277dcca826d11024e613
karen <kalaivani.dc@gmail.com>
2022-09-01 15:58:33 +0800

    myplay added

---
+++ myplay.php
@@ -0,0 +1,65 @@
+<?php include('header.php');?>
+
+     <div class="row">
+         <div class="col-12">
+            <div class="row">
+                <div class="col-8">
+                    <span style="color: #575758;">Hashies/EOS</span>
+                    <h3 style="color:#fff;">
+                        
+                        <img class="img-fluid" src="app-assets/images/home/gp.PNG" height="24" width="24">35.<span style="font-size: 12px;">00</span></h3>
+                        <span style="color: #575758;">Hashies Block</span>
+                        <span style="color:#fff;">#434217</span>
+                </div>
+
+                 <div class="col-4 text-right">
+                    <img class="img-fluid" src="app-assets/images/home/button_x.PNG" width="124">
+                        
+                        <span style="color:#575758;">11-08-2022 7:89pm</span>
+                </div>
+          </div>
+          <br/>
+
+
+           <div class="row">
+                <div class="col-8">
+                    <span style="color: #575758;">Hashies/TRX</span>
+                    <h3 style="color:#fff;">
+                        
+                        <img class="img-fluid" src="app-assets/images/home/gp.PNG" height="24" width="24">6.<span style="font-size: 12px;">00</span></h3>
+                        <span style="color: #575758;">Hashies Block</span>
+                        <span style="color:#fff;">#434217</span>
+                </div>
+
+                 <div class="col-4 text-right">
+                    <img class="img-fluid" src="app-assets/images/home/button_x.PNG" width="124">
+                        
+                        <span style="color:#575758;">11-08-2022 7:89pm</span>
+                </div>
+          </div>
+<br/>
+
+           <div class="row">
+                <div class="col-8">
+                     <span style="color: #575758;">Hashies/EOS</span>
+                    <h3 style="color:#fff;">
+                       
+                        <img class="img-fluid" src="app-assets/images/home/gp.PNG" height="24" width="24">57.<span style="font-size: 12px;">00</span></h3>
+                        <span style="color: #575758;">Hashies Block</span>
+                        <span style="color:#fff;">#434217</span>
+                </div>
+
+                 <div class="col-4 text-right">
+                    <img class="img-fluid" src="app-assets/images/home/button_x.PNG" width="124">
+                        
+                        <span style="color:#575758;">11-08-2022 7:89pm</span>
+                </div>
+          </div>
+          <br/>
+
+        </div>
+    </div>
+
+
+
+<?php include('footer.php');?>
