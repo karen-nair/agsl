d2e9fe8c4f5990a8d4b80e32f67e5f6d5cc55549
karen <kalaivani.dc@gmail.com>
2022-09-02 11:17:42 +0800

    navigation updated

---
+++ nav-menu.php
@@ -0,0 +1,44 @@
+  <nav class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-static-top navbar-brand-center" style="background-image: url('app-assets/images/home/border.PNG');background-size: contain;background-position-y:20px;">
+        <div class="navbar-header">
+            <ul class="nav navbar-nav flex-row">
+                <li class="nav-item">
+                    <a class="navbar-brand" href="index.php">
+                 <div class="brand-logo"><img class="logo" src="app-assets/images/home/logo.PNG"></div>
+                      
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-wrapper">
+            <div class="navbar-container content">
+                <div class="navbar-collapse" id="navbar-mobile">
+                    <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item mobile-menu mr-auto"><a class="nav-link nav-menu-main menu-toggle" href="javascript:void(0);"><img width="25" height="auto" src="app-assets/images/home/hppc.png"></a></li>
+                        </ul>
+                        
+                    </div>
+                    <ul class="nav navbar-nav float-right d-flex align-items-center">
+                        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-us"></i><span class="selected-language d-lg-inline d-none"></span></a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown-flag">
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    <i class="flag-icon flag-icon-us mr-50"></i>
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    <i class="flag-icon flag-icon-fr mr-50"></i>
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    <i class="flag-icon flag-icon-de mr-50"></i>
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    <i class="flag-icon flag-icon-pt mr-50"></i>
+                                </a>
+                            </div>
+                        </li>
+                        
+                       
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
