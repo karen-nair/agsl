0c47e8123e785db2b9f8a2b3137031a77798364f
karen <kalaivani.dc@gmail.com>
2022-09-09 22:27:39 +0800

    withdrawal page updated

--- withdrawal.php
+++ withdrawal.php
@@ -5,30 +5,36 @@ include('header.php');
 
      <div class="row">
          <div class="col-12 py-1">
-            <p class="grey-font">USDT Amount</p>
+            
+            <input class="trans-input" type="text" name="" placeholder="USDT Amount">
+
           </div>
       </div>
 
       <div class="row">
          <div class="col-12 py-1">
-            <p class="grey-font">USDT Wallet Address (TRC-20)</p>
+            
+            <input class="trans-input" type="text" name="" placeholder="USDT Wallet Address (TRC-20)">
+
           </div>
       </div>
 
       <div class="row">
          <div class="col-12 py-1">
-            <p class="grey-font">Security Password</p>
-          </div>
-      </div>
 
-     <div class="row">
-         <div class="col-6 text-center tab-blue" style="margin: 0 auto;">
-                <span class="unskewed">Submit</span>
+            <input class="trans-input" type="text" name="" placeholder="Security Password">
+
           </div>
       </div>
 
       <div class="row">
-         <div class="col-12 text-center py-4" style="margin: 0 auto;">
+         <div class="col-6 text-center tab-submit-green" style="margin: 0 auto;">
+                <span class="unskewed" style="position: relative;top: -2px;">Submit</span>
+          </div>
+      </div>
+
+      <div class="row  py-4">
+         <div class="col-12 text-center py-1 important">
             <p class="white-font">IMPORTANT</p>
             <p class="grey-font">Please note that withdrawals will be processed within 24 hours</p>
 
