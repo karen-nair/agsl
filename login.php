77b73d655c906e35747cd6d44e0064f5df4fcf6b
karen <kalaivani.dc@gmail.com>
2022-09-09 22:58:56 +0800

    login page updated

--- login.php
+++ login.php
@@ -1,57 +1,60 @@
 <?php include('header.php');?>
    
     <div class="row">
-         <div class="col-12">
+         <div class="col-12 login-wrapper">
             <h1 class="white-font">Hello!</h1>
             <h3 class="welcome">Welcome</h3>
           </div>
       </div>
 
-     <div class="row">
+     <div class="row py-1">
          <div class="col-12">
-            <p class="grey-font">Username</p>
-            <input class="trans-input" type="text" name="">
+           
+            <input class="trans-input" type="text" name="" placeholder="Username">
 
           </div>
       </div>
 
-      <div class="row">
-         <div class="col-10">
-            <p class="grey-font">Password</p>
-            <input class="trans-input" type="text" name="">
+      <div class="row py-1">
+         <div class="col-12">
+           
+            
+            <div class="pwd">
+               <input class="trans-input" type="text" name="" placeholder="Password">
+               <span class="p-viewer">
+                  <img class="img-fluid" src="app-assets/images/home/eye.PNG" height="24" width="24">
+               </span>
+            </div>
 
           </div>
 
-          <div class="col-2">
-            <img class="img-fluid" src="app-assets/images/home/eye.PNG" height="24" width="24">
-          </div>
+         
 
       </div>
 
-       <div class="row">
-         <div class="col-12 py-1 text-right">
+      
+      <div class="row py-1">
+         <div class="col-7">
+            <input class="trans-input" type="text" name="">
+
+          </div>
+          <div class="col-5 text-right">
             <img class="img-fluid" src="app-assets/images/home/captcha2.PNG" width="106" height="106">
 
           </div>
       </div>
+      <p class="grey-font">Forgot Password?</p>
 
-      <div class="row">
-         <div class="col-12">
-            <p class="grey-font">Forgot Password?</p>
-            <input class="trans-input" type="text" name="">
 
+      <div class="row py-2">
+         <div class="col-6 text-center tab-submit-green" style="margin: 0 auto;">
+                <span class="unskewed" style="position: relative;top: -2px;">Login</span>
           </div>
       </div>
 
-      <div class="row  py-2">
-         <div class="col-6 text-center tab-blue"  style="margin: 0 auto;">
-                <span class="unskewed">Login</span>
-          </div>
-      </div>
-
-      <div class="row  py-2">
-         <div class="col-6 text-center tab-blue" style="margin: 0 auto;">
-                <span class="unskewed">Register Account</span>
+      <div class="row py-2">
+         <div class="col-6 text-center tab-submit-yellow" style="margin: 0 auto;">
+                <span class="unskewed" style="position: relative;top: -2px;">Register Account</span>
           </div>
       </div>
 
