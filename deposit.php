8877931e32c764734dd1f16d1c3fea51c73f3f6e
karen <kalaivani.dc@gmail.com>
2022-09-22 14:58:29 +0800

    qr added on deposit  page

--- deposit.php
+++ deposit.php
@@ -4,6 +4,30 @@ include('header.php');
 ?>
 <div class="card form-wrapper">
    <div class="card-body">
+   <div class="row">
+        <div class="col-12 text-center">
+              <p>Pay To</p>
+           <img class="img-fluid qr" src="app-assets/images/home/qr.png">
+
+        </div>
+      </div>
+   
+    <div class="row  py-2">
+        <div class="col-md-7 col-sm-12 text-center">
+
+           <div class="wrapper"> 
+                 <input class="trans-input" type="text" name="" value="94dd78149a2f3f5ca501f86a7788a36d">
+              </div>
+
+        </div>
+         <div class="col-md-5 col-sm-12 text-center green-yellow-btn-wrapper">
+              <div class="tab-submit-yellow btn-center-align hash-copy">
+                  <span class="unskewed">Copy</span>
+                 
+              </div>
+            </div>
+     </div>
+
      <div class="row">
          <div class="col-12 py-1">
 
